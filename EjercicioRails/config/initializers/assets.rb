eb36bd149b1c8060f4feca65e906c5ef97686caf
felipe vera <felipevera10@gmail.com>
2017-08-22 16:07:24 -0300

    pagina

--- EjercicioRails/config/initializers/assets.rb
+++ EjercicioRails/config/initializers/assets.rb
@@ -5,13 +5,10 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
-
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
-
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
-
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( *.css *.js *.otf *.eot *.svg *.ttf *.woff *.woff2 *.jpg *.png *.)
+Rails.application.config.assets.precompile += %w( *.css *.js *.otf *.eot *.svg *.ttf *.woff *.woff2 *.jpg *.png)
